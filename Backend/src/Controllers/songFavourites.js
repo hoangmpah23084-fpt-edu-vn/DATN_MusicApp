2ae6c6adbc18c64b46ca8ea0076f0135bcf03556
hoanganh203 <105597730+hoanganh203@users.noreply.github.com>
2023-07-19 21:58:13 +0700

    feat:MUSIC-16 Creat_songFavourites refs:MUSIC-16

--- Backend/src/Controllers/songFavourites.js
+++ Backend/src/Controllers/songFavourites.js
@@ -2,12 +2,17 @@ import Favourites from "../Models/songFavourites";
 
 //========= getFavourites ================
 
-export const getFavourites = async () => {
-    id_user = req.params.id_user // lấy ra id của user từ req
+export const getFavourites = async (req, res) => {
     try {
-        const list_songFavourites = await Favourites.findOne({ id_user }).populate('list_songFavourites.id_song')
+        const list_songFavourites = await Favourites.findOne({ id_user: req.params.id_user }).populate('list_songFavourites.id_song')
         // truy cập vào bảng Favourites điều kiện id_user ? truy cập vào list_songFavourites để lấy id_song : catch
-        return res.status(200).json({
+
+        if (!list_songFavourites) {
+            return res.status(201).json({
+                message: "Danh sách không tồn tại",
+            });
+        }
+        return res.status(201).json({
             message: "Danh sách Favourites",
             list_songFavourites,
         });
@@ -17,4 +22,42 @@ export const getFavourites = async () => {
             // gửi error 
         });
     }
+}
+
+//=================end Get =========================
+
+//=================CreatFavourites =========================
+
+export const createFavourites = async (req, res) => {
+
+    const { id_user, id_song } = req.body; // lấy id_user và id_song từ request
+
+    // tạo 1 object có chưa id_bài hát để cập nhật lại danh sách
+    const list_songFavourite = {
+        id_song,
+    }
+    try {
+        // bắt đầu call API
+        const dataFavourite = await Favourites.findOneAndUpdate(
+            { id_user }, //tham số đầu tiên là id_user
+            { $addToSet: { list_songFavourites: list_songFavourite } }, // tham số thứ 2 dùng addToset để add list_songFavourite vào list_songFavourites
+            { upsert: true, new: true } // này chưa biết là gì 
+        )
+
+        if (!dataFavourite) {
+            return res.status(201).json({
+                message: "Thêm không thành công"
+            })
+        }
+
+        return res.status(201).json({
+            message: "Thêm thành công",
+            dataFavourite
+        })
+    } catch (error) {
+        return res.status(404).json({
+            message: error.message,
+        })
+    }
+
 }
--- Backend/src/Routers/songFavourites.js
+++ Backend/src/Routers/songFavourites.js
@@ -1,9 +1,11 @@
 import express from "express"
-import { getFavourites } from "../Controllers/songFavourites";
+import { createFavourites, getFavourites } from "../Controllers/songFavourites";
 
 
 const router = express.Router();
 
-router.get("/songFavourites", getFavourites)
+router.get("/songFavourites/:id_user", getFavourites)
+router.post("/songFavourites", createFavourites)
+
 
 export default router
