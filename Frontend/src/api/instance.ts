ceb9753813419de9cf731b014b3f48c7ea12f125
Hello <Thang@gmail.com>
2023-07-31 23:24:34 +0700

    [FE] Create index

--- Frontend/src/api/instance.ts
+++ Frontend/src/api/instance.ts
@@ -1,18 +1,18 @@
-import axios, { AxiosInstance } from 'axios';
+// import axios, { AxiosInstance } from 'axios';
 
-class Http {
-	instance: AxiosInstance;
-	constructor() {
-		this.instance = axios.create({
-			baseURL: 'http://localhost:8080/api',
-			timeout: 10000,
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		});
-	}
-}
+// class Http {
+// 	instance: AxiosInstance;
+// 	constructor() {
+// 		this.instance = axios.create({
+// 			baseURL: 'http://localhost:8080/api',
+// 			timeout: 10000,
+// 			headers: {
+// 				'Content-Type': 'application/json',
+// 			},
+// 		});
+// 	}
+// }
 
-const http = new Http().instance;
+// const http = new Http().instance;
 
-export default http;
+// export default http;
