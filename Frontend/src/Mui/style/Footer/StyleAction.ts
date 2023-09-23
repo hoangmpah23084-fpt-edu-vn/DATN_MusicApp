354a7813b62364e7d854c4fff445a1537fcbc5b3
Hello <Thang@gmail.com>
2023-09-23 16:59:29 +0700

    MUSIC-35 Handle listen to music

---
+++ Frontend/src/Mui/style/Footer/StyleAction.ts
@@ -0,0 +1,67 @@
+import { ListItemButton, ListItemIcon, styled } from "@mui/material";
+
+const ListItemButtonStyle = styled(ListItemButton)({
+    width : "46px",
+    height : "100%",
+    borderRadius : "50px",
+    transition : "0.5s",
+    display : "flex",
+    justifyContent : "center",
+    alignItems : "center",
+    " .MuiTouchRipple-root" : {
+        width : "37px",
+        height : "36px",
+        transform: "translate(3px, 4px)",
+    }
+});
+const ListItemIconStyle = styled(ListItemIcon)({
+    minWidth : "0",
+    width : "37px",
+    height : "36px",
+    borderRadius : "50px",
+    transition : "0.5s",
+    display : "flex",
+    justifyContent : "center",
+    alignItems : "center",
+    ":hover" : { 
+        backgroundColor: "rgb(187 184 184 / 31%)", 
+    }
+});
+const PauseListItemButtonStyle = styled(ListItemButton)({
+    width : "60px",
+    height : "100%",
+    borderRadius : "50px",
+    transition : "0.5s",
+    display : "flex",
+    justifyContent : "center",
+    alignItems : "center",
+    " .MuiTouchRipple-root" : {
+        width : "40px",
+        height : "40px",
+        transform: "translate(10px, 4px)",
+    }
+});
+const PauseListItemIconStyle = styled(ListItemIcon)({
+    minWidth : "0",
+    width : "40px",
+    height : "40px",
+    borderRadius : "50px",
+    display : "flex",
+    justifyContent : "center",
+    alignItems : "center",
+    border : "2px solid white",
+    ":hover" :{
+        border : "2px solid #9B4DE0",
+        "& .MuiSvgIcon-root" : {
+            color : "#9B4DE0"
+        }
+    }
+});
+
+
+export {
+    ListItemButtonStyle,
+    ListItemIconStyle,
+    PauseListItemButtonStyle,
+    PauseListItemIconStyle
+}
