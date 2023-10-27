56e5424e52e589f888918bd768b9148810bb95eb
JJthang <thangpdph@gmail.com>
2023-10-27 10:48:24 +0700

    [FE] List data in SideBar Song

--- Frontend/src/components/Footer/index.tsx
+++ Frontend/src/components/Footer/index.tsx
@@ -15,7 +15,6 @@ import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
 import { SongStateContext } from "../Context/SongProvider";
 import { NextSong, PrevSong } from "./NextSong";
 import { ifSong } from "@/pages/Admin/Interface/ValidateSong";
-import io from "socket.io-client";
 
 const connect = io("http://localhost:8080")
 export const useStyles = makeStyles(() => createStyles({
@@ -43,7 +42,6 @@ const Footer = (props : Props) => {
   const classes = useStyles();
   const [intervalId, setIntervalId] = useState<number | null>(null);
   const {setGlobalPause, globalPause } = SongStateContext();
-  const [tryConnect, setTryConnect] = useState(false);
   
   const togglePlayPause = useCallback(() => {
     const preValue = globalPause;
@@ -94,8 +92,6 @@ const Footer = (props : Props) => {
   
   useEffect(() => {
     globalPause ? audioRef.current?.play() : audioRef.current?.pause();
-    connect.emit("trySend", [1,2,3])
-    connect.on("connected", () => void setTryConnect(true));
     if (globalPause) {
       const id = setInterval(() => {
         audioRef.current && setRewindAudio(audioRef.current?.currentTime);
