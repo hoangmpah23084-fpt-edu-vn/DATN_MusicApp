c530747ff5f7c382d0ac59da5952fe69cf2aa696
LeManh <manhzxc3@gmail.com>
2023-10-31 22:05:08 +0700

    fix: giao diện author

--- Frontend/src/pages/Admin/Interface/validateAuth.ts
+++ Frontend/src/pages/Admin/Interface/validateAuth.ts
@@ -1,14 +1,14 @@
 import * as Yup from "yup"
 export const SignupSchema = Yup.object({
-    fullName: Yup.string().required("First name is required"),
-    email: Yup.string().email("Invalid email").required("Email is required"),
-    password: Yup.string().min(6).required("Password is required"),
-    confirmPassword: Yup.string().required("require").oneOf([Yup.ref("password")], "Passwords must match"),
+    fullName: Yup.string().required("Tên là bắt buộc"),
+    email: Yup.string().email("Hãy nhập đúng định dạng email").required("Email là bắt buộc"),
+    password: Yup.string().min(6, "Hãy nhập trên 6 ký tự").required("Password là bắt buộc"),
+    confirmPassword: Yup.string().required("Password là bắt buộc").oneOf([Yup.ref("password")], "Passwords phải trùng nhau"),
 })
 export type SignupForm = Yup.InferType<typeof SignupSchema>
 
 export const SigninSchema = Yup.object({
-    email: Yup.string().email("Invalid email").required("Email is required"),
-    password: Yup.string().min(6).required("Password is required"),
+    email: Yup.string().email("Hãy nhập đúng định dạng email").required("Email là bắt buộc"),
+    password: Yup.string().min(6, "Hãy nhập trên 6 ký tự").required("Password là bắt buộc"),
 })
 export type SigninForm = Yup.InferType<typeof SigninSchema>
