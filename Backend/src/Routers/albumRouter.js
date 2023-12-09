a4903d417ad97d7e89a0b827a61ab76d3700fe14
quan44652 <quan44652@gmail.com>
2023-12-09 15:38:01 +0700

    update code

--- Backend/src/Routers/albumRouter.js
+++ Backend/src/Routers/albumRouter.js
@@ -5,7 +5,7 @@ const AlbumRouter = express.Router();
 AlbumRouter.get("/album", getAll_Album);
 AlbumRouter.get("/album/:id", get_AlbumById);
 AlbumRouter.post("/album", create_Album);
-AlbumRouter.patch("/album/:id", update_Album);
+AlbumRouter.put("/album/:id", update_Album);
 AlbumRouter.delete("/album/:id", delete_Album);
 
 export default AlbumRouter;
