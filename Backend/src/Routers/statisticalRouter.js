be79ea81467be76f73e36d1c964ec757954eee57
quan44652 <quan44652@gmail.com>
2023-12-20 23:36:29 +0700

    update code thống kê

--- Backend/src/Routers/statisticalRouter.js
+++ Backend/src/Routers/statisticalRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { monthSong, statistical } from '../Controllers/statisticalController.js'
+import { monthSong, monthUser, statistical } from '../Controllers/statisticalController.js'
 
 const router = express.Router()
 
 
 router.post("/statistical",statistical)
-router.post("/statistical/month-song",monthSong)
+router.get("/statistical/month-song",monthSong)
+router.get("/statistical/month-user",monthUser)
 
 export default router;
